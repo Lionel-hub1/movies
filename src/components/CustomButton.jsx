f19c1399402bcb90a7042fdef6c795dcbd7f97f2
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-03-25 08:58:55 +0200

    Add CustomButton component

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+import { ICONS } from "../data/constants";
+
+const CustomButton = ({ value }) => {
+  return (
+    <div>
+      <button className="flex border-2 border-white text-white font-bold text-2xl py-2 px-4 rounded-lg hover:bg-primaryDark hover:text-white transition duration-300 ease-in-out">
+        <img className="h-10 w-10" src={ICONS.playIc} alt="" />
+        <span className="text-3xl">{value}</span>
+      </button>
+    </div>
+  );
+};
+
+CustomButton.propTypes = {
+  value: PropTypes.string,
+};
+
+export default CustomButton;
