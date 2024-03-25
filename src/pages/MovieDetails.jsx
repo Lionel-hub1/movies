bf8adad67aa73b041f390374a567ae6bc55f9d25
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-03-25 22:33:07 +0200

    Add YouTube component and update movie details layout

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 
 const MovieDetails = () => {
   const { id } = useParams();
@@ -40,25 +41,65 @@ const MovieDetails = () => {
   return (
     <>
       {loading ? (
-        <div className="h-full animate-ping text-white w-screen flex justify-center items-center">
+        <div className="h-full animate-ping text-white flex justify-center items-center">
           <h1 className="text-3xl">Loading...</h1>
         </div>
       ) : (
-        <div className="min-h-screen w-screen text-white flex justify-center items-center">
-          <div className="flex">
-            <img
-              className="w-1/2"
-              src={`http://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
-              alt=""
+        <div className="relative w-full px-14 text-white flex justify-center items-center">
+          <img
+            className="w-1/2 absolute hidden"
+            src={`http://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+            alt=""
+          />
+          <div className={"flex bg-[url()]"}>
+            <YouTube
+              className="z-50 w-1/2"
+              videoId={movieVideo}
+              opts={{
+                height: "590",
+                width: "100%",
+                playerVars: {
+                  autoplay: 0,
+                  controls: 0,
+                  modestbranding: 1,
+                  loop: 1,
+                },
+              }}
             />
-            <div className="w-1/2 px-4 flex flex-col">
-              <video
-                className="h-32"
-                src={`https://www.youtube.com/watch?v=${movieVideo}`}
-                controls
-              />
-              <h1 className="text-9xl font-bold">{movieDetails.title}</h1>
-              <p className="text-4xl">{movieDetails.overview}</p>
+            <div className="w-1/2 flex flex-col px-5">
+              <h1 className="text-7xl font-bold font-['Bebas_Neue']">
+                {movieDetails.title}
+              </h1>
+              {/* Genres */}
+              <div className="flex space-x-3">
+                {movieDetails.genres.map((genre, index) => (
+                  <span
+                    key={index}
+                    className="py-1 px-3 text-lg font-['Inter'] text-headText bg-slate-400 bg-opacity-55 rounded"
+                  >
+                    #{genre.name}
+                  </span>
+                ))}
+              </div>
+              <p className="text-2xl font-['Inter'] text-bodyText py-4">
+                {movieDetails.overview}
+              </p>
+              <span className="text-xl font-['Inter'] text-headText">
+                Runtime: {movieDetails.runtime} minutes
+              </span>
+              <span className="text-xl font-['Inter'] text-headText">
+                Release Date: {movieDetails.release_date}
+              </span>
+              <div className="flex h-full justify-between items-center mt-5">
+                <span className="space-x-3">
+                  <span className="text-lg text-primary">votes:</span>
+                  <span className="text-3xl">{movieDetails.vote_count}</span>
+                </span>
+                <span className="space-x-3">
+                  <span className="text-lg text-primary">Rating:</span>
+                  <span className="text-3xl">{movieDetails.vote_average}</span>
+                </span>
+              </div>
             </div>
           </div>
         </div>
