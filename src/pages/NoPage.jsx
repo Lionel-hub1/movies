84e63c99722b4e41d8c738acaaafd1acbd7a9767
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-03-25 15:48:04 +0200

    Update NoPage component styling

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,8 +1,10 @@
 const NoPage = () => {
   return (
-    <div className="flex flex-col text-center py-32">
+    <div className="flex flex-col text-center py-32 h-screen">
       <span className="text-primary text-xl font-bold">404</span>
-      <span className="text-5xl text-white font-black py-2">Page not found</span>
+      <span className="text-5xl text-white font-black py-2">
+        Page not found
+      </span>
       <span className="text-lg text-bodyText">
         Sorry! We couldn&apos;t find the page you&apos;re looking for.
       </span>
