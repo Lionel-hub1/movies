cc93aff1407c25b951d3f03c08e2625891d03efb
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2025-08-31 15:05:20 +0200

    refactor: Remove unused top image assets from project

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,8 +1,6 @@
 import logo from "../assets/icons/movies-logo.svg";
 import brand from "../assets/icons/movies-word.svg";
 import topBackground from "../assets/images/top-background.png";
-import topFrontImg from "../assets/images/top-front-img.png";
-import topBackImg from "../assets/images/top-back-img.png";
 import playIc from "../assets/icons/play_circle.svg";
 import fireIc from "../assets/icons/fire.svg";
 import likeIc from "../assets/icons/like.svg";
@@ -13,8 +11,6 @@ export const ICONS = {
     logo,
     brand, 
     topBackground,
-    topFrontImg,
-    topBackImg,
     playIc,
     fireIc,
     likeIc,
